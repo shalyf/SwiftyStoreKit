f86a0674ee72fda5c24a857c41b57482add8b850
Easher Yii <yii@Eashers-Mac-mini-2023.local>
2023-11-09 15:50:46 +0800

    increase the minimum develop target

--- Package.swift
+++ Package.swift
@@ -3,7 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyStoreKit",
-    platforms: [.iOS("9.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("6.2")],
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_14),
+        .tvOS(.v9),
+        .watchOS(.v5)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
